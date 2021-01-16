a9a1e19eec697b39fcd9d76baa65a7f9ad2f5b90
Julia Kim <juliaykim6@gmail.com>
2021-01-16 01:37:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,4 @@
 def oxford_comma(array)
   if array.length = 2
     return array.join(array << "and")
-  elsif
-    array.length = 3
-    return array.join("")
-  else
-    return array.join(",and")
-end
-end
-end
 end
