143effacafabc55019c04a747a81dfb9f0cfa85f
Julia Kim <juliaykim6@gmail.com>
2021-01-16 01:43:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,5 @@
 def oxford_comma(array)
-  if array.length == 2
-    array.join(" and ")
-  elsif array.length > 2
-    array.insert(-2, and)
-    array[0..-1].join(", ")
-  else
-    array.join
-  end
-end
+  return array.join(" and ") if array.size < 3
+  array [-1] = "and" + list[-1]
+  list.join(", ")
+    
